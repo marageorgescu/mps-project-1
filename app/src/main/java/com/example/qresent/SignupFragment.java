73df8620f13f02bb4951969e37d0ea2f7bd901d0
Andrei-Mocanu <andreic.mocanu@gmail.com>
2021-11-13 22:27:34 +0200

    Updated users table in database

--- app/src/main/java/com/example/qresent/SignupFragment.java
+++ app/src/main/java/com/example/qresent/SignupFragment.java
@@ -20,9 +20,15 @@ import com.example.qresent.databinding.FragmentLoginBinding;
 import com.example.qresent.databinding.FragmentSignupBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class SignupFragment extends Fragment {
 
@@ -38,6 +44,7 @@ public class SignupFragment extends Fragment {
                 false
         );
 
+        mAuth = FirebaseAuth.getInstance();
         changeHighlightButton(binding.studentBtn, binding);
 
         binding.studentBtn.setOnClickListener(v -> changeHighlightButton(binding.studentBtn, binding));
@@ -47,7 +54,11 @@ public class SignupFragment extends Fragment {
         binding.signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                createAccount(binding.emailSignUpET.getText().toString(), binding.passwordSignUpET.getText().toString(), v);
+                createAccount(binding.emailSignUpET.getText().toString(),
+                        binding.passwordSignUpET.getText().toString(),
+                        binding.nameET.getText().toString(),
+                        binding.grupaET.getText().toString(),
+                        binding.facultateET.getText().toString(),v);
             }
         });
 
@@ -76,14 +87,33 @@ public class SignupFragment extends Fragment {
         }
     }
 
-    private void createAccount(String email, String password, View v) {
-        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
-        }).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                Navigation.findNavController(v)
-                        .navigate(R.id.action_signupFragment_to_qrGeneratorFragment);
-            }
+    private void createAccount(String email, String password,String name,String group,
+                               String faculty,View v) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        Navigation.findNavController(v)
+                                .navigate(R.id.action_signupFragment_to_qrGeneratorFragment);
+
+                        HashMap<Object, String> hashMap = new HashMap<>();
+                        hashMap.put("uid", mAuth.getCurrentUser().getUid());
+                        hashMap.put("name", name);
+                        hashMap.put("grupa", group);
+                        hashMap.put("grupa", faculty);
+                        hashMap.put("email", email);
+
+
+
+                        //Firebase database instance
+                        FirebaseDatabase database = FirebaseDatabase.getInstance("https://qresent-926c3-default-rtdb.europe-west1.firebasedatabase.app/");
+                        DatabaseReference myRef = database.getReference("Users");
+                        //put data within hashmap in database
+                        myRef.child(mAuth.getCurrentUser().getUid()).setValue(hashMap);
+
+
+                    }
+
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
